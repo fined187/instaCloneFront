ecf756ffb348cbcda13c991da1c097502db6433a
fined000@naver.com <fined000@naver.com>
2023-03-28 23:18:24 +0900

    set up commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./screens/Home";
+import Login from "./screens/Login";
+import NotFound from "./screens/NotFound";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          {isLoggedIn ? (
+          <Home setIsLoggedIn={setIsLoggedIn} />
+          ) : (
+          <Login setIsLoggedIn={setIsLoggedIn} />
+          )}
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/screens/Login.js
@@ -0,0 +1,9 @@
+const Login = ({setIsLoggedIn}) => {
+  return(
+    <div>
+      <h1>Login</h1>
+      <button onClick={() => setIsLoggedIn(true)}>Log in now</button>
+    </div>
+  )
+};
+export default Login;
